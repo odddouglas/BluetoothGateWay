674711cd67e5b6ffe5ea28224a6d2a4ee7841db0
odddouglas <18718048671iota@gmail.com>
2025-04-05 01:20:25 +0800

    add: 基础页面样式已完成,包含Switch开关，Cell单元格，Divider分割线

--- miniprogram/miniprogram/pages/index/index.ts
+++ miniprogram/miniprogram/pages/index/index.ts
@@ -1,1 +1,13 @@
-Component({});
+Component({
+    data: {
+      defaultVal: true,
+    },
+    methods: {
+      handleChange(e) {
+        this.setData({
+          defaultVal: e.detail.value,
+        });
+      },
+    },
+  });
+  
