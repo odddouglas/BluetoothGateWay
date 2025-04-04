52686d1a29950bf7a214951416c57ced2717c218
odddouglas <18718048671iota@gmail.com>
2025-04-04 18:26:59 +0800

    fix: 规整代码风格

--- main/app_main.c
+++ main/app_main.c
@@ -17,8 +17,6 @@
 #define LED_GPIO 48 // LED 数据线连接的 GPIO
 #define LED_NUM 1   // LED 灯带上的 LED 数量
 
-static led_strip_handle_t led_strip;
-
 #define DEVICE_ID "67ed58015367f573f77ef961_esp32"
 #define DEVICE_SECRET "1dd8ae3b5de51602871d8039dabf6f9e"
 #define CLIENT_ID "67ed58015367f573f77ef961_esp32_0_0_2025040406"
@@ -29,6 +27,26 @@ static led_strip_handle_t led_strip;
 // 上报属性的主题宏定义
 #define TOPIC_PROPERTIES_REPORT "$oc/devices/%s/sys/properties/report"
 
+// led_strip配置
+led_strip_config_t strip_config = {
+    .strip_gpio_num = LED_GPIO,
+    .max_leds = LED_NUM,
+};
+led_strip_rmt_config_t rmt_config = {
+    .resolution_hz = 10 * 1000 * 1000, // 10 MHz
+    .flags.with_dma = false,
+};
+static led_strip_handle_t led_strip;
+
+// 配置MQTT客户端（非加密）
+esp_mqtt_client_config_t mqtt_cfg = {
+    .broker.address.uri = BROKER_URI,
+    .credentials = {
+        .client_id = CLIENT_ID,
+        .username = DEVICE_ID,
+        .authentication.password = CLIENT_SECRET,
+    },
+};
 
 static void publish_sensor_data(esp_mqtt_client_handle_t client,
                                 float temp, float humi, bool led)
@@ -63,7 +81,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
     {
     case MQTT_EVENT_CONNECTED:
         ESP_LOGI(TAG, "Connected to Huawei IoT Platform");
-        
+
         publish_sensor_data(event->client, 22.5, 100.0, true);
         break;
 
@@ -99,38 +117,15 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
 
 void app_main()
 {
-    // LED 配置
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = LED_GPIO,
-        .max_leds = LED_NUM,
-    };
-
-    // RMT 配置
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10 MHz
-        .flags.with_dma = false,
-    };
     // 初始化 LED 灯带
     ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-
     // 初始化基础组件
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
     // 连接WiFi
     ESP_ERROR_CHECK(example_connect());
-
-    // 配置MQTT客户端（非加密）
-    esp_mqtt_client_config_t mqtt_cfg = {
-        .broker.address.uri = BROKER_URI,
-        .credentials = {
-            .client_id = CLIENT_ID,
-            .username = DEVICE_ID,
-            .authentication.password = CLIENT_SECRET,
-        },
-    };
-
+    // 初始化 MQTT
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
@@ -140,10 +135,10 @@ void app_main()
     while (1)
     {
         // 设置红色
-        ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, 0, 255, 0, 0)); // 红色 (255, 0, 0)
-        ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+        led_strip_set_pixel(led_strip, 0, 255, 0, 0); // 红色 (255, 0, 0)
+        led_strip_refresh(led_strip);
         ESP_LOGI(TAG, "LED ON");
-        
+
         vTaskDelay(10000 / portTICK_PERIOD_MS);
 
         // 生成模拟数据
