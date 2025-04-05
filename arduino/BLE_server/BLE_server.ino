36d09deebaf315e6913e69e71429635659d443d8
odddouglas <18718048671iota@gmail.com>
2025-04-06 00:19:40 +0800

    fix: 添加注释

--- arduino/BLE_server/BLE_server.ino
+++ arduino/BLE_server/BLE_server.ino
@@ -3,23 +3,24 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+// 蓝牙相关定义
 BLECharacteristic *pCharacteristic;
 bool deviceConnected = false;
 char BLEbuf[32] = {0};
 uint32_t cnt = 0;
 
-#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"           // UART服务UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // 接收特征UUID
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" // 发送特征UUID
 
-boolean doScan = true;
-boolean doConnect = false;
-boolean connected = false;
+boolean doScan = true;     // 是否开始扫描设备
+boolean doConnect = false; // 是否连接设备
+boolean connected = false; // 设备是否已连接
 
-BLEAdvertisedDevice *pServer = nullptr;
-BLERemoteCharacteristic *pRemoteCharacteristic = nullptr;
-BLERemoteCharacteristic *pRemoteCharacteristic_2 = nullptr;
-BLEClient *pClient = nullptr;
+BLEAdvertisedDevice *pServer = nullptr;                     // 存储找到的设备
+BLERemoteCharacteristic *pRemoteCharacteristic = nullptr;   // 存储远程读取特征
+BLERemoteCharacteristic *pRemoteCharacteristic_2 = nullptr; // 存储远程写入特征
+BLEClient *pClient = nullptr;                               // 客户端实例
 
 // 搜索到设备时回调功能
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
@@ -49,11 +50,11 @@ public:
         // 设置MTU大小
         if (pClient->setMTU(512))
         {
-            Serial.println("MTU set successfully.");
+            Serial.println("MTU设置成功.");
         }
         else
         {
-            Serial.println("Failed to set MTU.");
+            Serial.println("MTU设置失败.");
         }
     }
 
@@ -65,7 +66,7 @@ public:
 
         if (pClient)
         {
-            delete pClient;
+            delete pClient; // 删除客户端对象
             pClient = nullptr;
         }
     }
@@ -74,23 +75,23 @@ public:
 // 收到服务推送的数据时的回调函数
 void NotifyCallback(BLERemoteCharacteristic *pBLERemoteCharacteristic, uint8_t *pData, size_t length, bool isNotify)
 {
-    int numValues = length / sizeof(int16_t);
-    int16_t *dataArr = (int16_t *)pData;
-    Serial.printf("Received %d int16_t values:\n", numValues);
-    Serial.printf("Value  %s\n", dataArr);
+    int numValues = length / sizeof(int16_t); // 计算接收到的int16_t数量
+    int16_t *dataArr = (int16_t *)pData;      // 将数据转换为int16_t类型的数组
+    Serial.printf("接收到 %d 个int16_t值:\n", numValues);
+    Serial.printf("值: %s\n", dataArr); // 打印收到的值
 }
 
 // 用来连接设备获取其中的服务与特征
 bool ConnectToServer()
 {
-    pClient = BLEDevice::createClient();
+    pClient = BLEDevice::createClient(); // 创建客户端实例
     if (!pClient)
     {
         Serial.println("创建客户端失败");
         return false;
     }
 
-    pClient->setClientCallbacks(new MyClientCallback()); // 添加客户端与服务器连接与断开回调功能
+    pClient->setClientCallbacks(new MyClientCallback()); // 添加客户端连接与断开回调
 
     if (!pClient->connect(pServer))
     { // 尝试连接设备
@@ -100,7 +101,8 @@ bool ConnectToServer()
         return false;
     }
 
-    BLERemoteService *pRemoteService = pClient->getService(SERVICE_UUID); // 获取设备中的服务
+    // 获取设备中的服务
+    BLERemoteService *pRemoteService = pClient->getService(SERVICE_UUID);
     if (!pRemoteService)
     {
         Serial.println("获取服务失败");
@@ -108,6 +110,7 @@ bool ConnectToServer()
         return false;
     }
 
+    // 获取服务中的特征
     pRemoteCharacteristic = pRemoteService->getCharacteristic(CHARACTERISTIC_UUID_TX);
     pRemoteCharacteristic_2 = pRemoteService->getCharacteristic(CHARACTERISTIC_UUID_RX);
 
@@ -127,7 +130,7 @@ bool ConnectToServer()
 
     if (pRemoteCharacteristic->canNotify())
     {
-        pRemoteCharacteristic->registerForNotify(NotifyCallback);
+        pRemoteCharacteristic->registerForNotify(NotifyCallback); // 注册通知回调函数
     }
 
     return true;
@@ -141,10 +144,10 @@ void setup()
     BLEDevice::init("");
 
     BLEScan *pBLEScan = BLEDevice::getScan();
-    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-    pBLEScan->setActiveScan(true);
-    pBLEScan->setInterval(100);
-    pBLEScan->setWindow(80);
+    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks()); // 设置设备扫描回调
+    pBLEScan->setActiveScan(true);                                             // 开启主动扫描
+    pBLEScan->setInterval(100);                                                // 扫描间隔
+    pBLEScan->setWindow(80);                                                   // 扫描窗口
 }
 
 int count = 0;
@@ -154,8 +157,8 @@ void loop()
     if (doScan)
     {
         Serial.println("开始搜索设备");
-        BLEDevice::getScan()->clearResults();
-        BLEDevice::getScan()->start(0); // 持续搜索设备
+        BLEDevice::getScan()->clearResults(); // 清除上次扫描结果
+        BLEDevice::getScan()->start(0);       // 持续搜索设备
     }
 
     // 如果找到设备就尝试连接
@@ -163,11 +166,11 @@ void loop()
     {
         if (ConnectToServer())
         {
-            connected = true;
+            connected = true; // 设置连接状态
         }
         else
         {
-            doScan = true;
+            doScan = true; // 重新开始扫描
         }
         doConnect = false;
     }
@@ -175,18 +178,24 @@ void loop()
     // 如果已经连接，可以向设备发送数据
     if (connected && pRemoteCharacteristic && pRemoteCharacteristic_2 && pRemoteCharacteristic_2->canWrite())
     {
-        String newValue = "10";
+        String newValue = "10"; // 默认发送的值
         if (count == 1)
-            newValue = "00";
+        {
+            newValue = "00"; // 发送其他值
+        }
         else if (count == 2)
-            newValue = "01";
+        {
+            newValue = "01"; // 发送其他值
+        }
 
-        Serial.printf("像特征写入消息: %s\r\n", newValue.c_str());
-        pRemoteCharacteristic_2->writeValue(newValue.c_str(), newValue.length());
+        Serial.printf("向特征写入消息: %s\r\n", newValue.c_str());
+        pRemoteCharacteristic_2->writeValue(newValue.c_str(), newValue.length()); // 写入数据到设备
 
         delay(3500); // 控制发送间隔
-        count++;
+        count++;     // 增加计数
         if (count > 2)
-            count = 0;
+        {
+            count = 0; // 重置计数
+        }
     }
 }
--- arduino/BluetoothNode/BluetoothNode.ino
+++ arduino/BluetoothNode/BluetoothNode.ino
@@ -5,54 +5,63 @@
 #include <DHT.h>
 #include <DHT_U.h>
 
-/******************* 温湿度 **********************/
-#define DHTTYPE DHT11 // DHT 11
-#define DHTPIN 2      // GPI02
+/******************* 温湿度传感器设置 **********************/
+#define DHTTYPE DHT11 // 使用 DHT11 温湿度传感器
+#define DHTPIN 2      // 温湿度传感器连接到 GPIO2
 
-DHT dht(DHTPIN, DHTTYPE, 15);
-uint8_t flag_dht = 0;
-uint8_t flag_led = 0;
-uint8_t flag_send_dht = 0;
-float dht_buf[2];
+DHT dht(DHTPIN, DHTTYPE, 15); // 初始化 DHT11 传感器
+uint8_t flag_dht = 0;         // 控制是否读取温湿度传感器的标志位
+uint8_t flag_led = 0;         // 控制是否点亮LED的标志位
+uint8_t flag_send_dht = 0;    // 控制是否发送温湿度数据的标志位
+float dht_buf[2];             // 存储湿度和温度数据的数组
 
 /*****************************************/
 
-/******************* 蓝牙 **********************/
-BLECharacteristic *pCharacteristic;
-bool deviceConnected = false;
-char BLEbuf[32] = {0};
+/******************* 蓝牙设置 **********************/
+BLECharacteristic *pCharacteristic; // 用于发送数据的BLE特征
+bool deviceConnected = false;       // 设备是否连接
+char BLEbuf[32] = {0};              // 用于存储发送的蓝牙数据
 
-#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
+#define SERVICE_UUID "6E400001-B5A3-F393-E0A9-E50E24DCCA9E"           // UART服务的UUID
+#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E" // 接收特征的UUID
+#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E" // 发送特征的UUID
 
+// BLE服务器连接回调
 class MyServerCallbacks : public BLEServerCallbacks
 {
-    void onConnect(BLEServer *pServer) { deviceConnected = true; }
-    void onDisconnect(BLEServer *pServer) { deviceConnected = false; }
+    void onConnect(BLEServer *pServer)
+    {
+        deviceConnected = true; // 设备连接时设置连接状态为真
+    }
+    void onDisconnect(BLEServer *pServer)
+    {
+        deviceConnected = false; // 设备断开时设置连接状态为假
+    }
 };
 
+// BLE特征写入回调
 class MyCallbacks : public BLECharacteristicCallbacks
 {
     void onWrite(BLECharacteristic *pCharacteristic)
     {
-        std::string rxValue = pCharacteristic->getValue();
+        std::string rxValue = pCharacteristic->getValue(); // 获取写入的值
         if (rxValue.length() > 0)
         {
             Serial.print("------> Received Value: ");
             for (int i = 0; i < rxValue.length(); i++)
             {
-                Serial.print(rxValue[i]);
+                Serial.print(rxValue[i]); // 打印接收到的值
             }
             Serial.println();
 
+            // 判断接收到的指令并设置标志位
             if (rxValue.find("00") != -1)
             {
-                flag_dht = 1;
+                flag_dht = 1; // 设置读取DHT传感器的标志位
             }
             else if (rxValue.find("01") != -1)
             {
-                flag_led = 1;
+                flag_led = 1; // 设置点亮LED的标志位
             }
         }
     }
@@ -62,70 +71,76 @@ class MyCallbacks : public BLECharacteristicCallbacks
 /****************** 主程序 ***********************/
 void setup()
 {
-    Serial.begin(115200);
+    Serial.begin(115200); // 初始化串口通信
 
-    pinMode(12, OUTPUT);
-    dht.begin();
+    pinMode(12, OUTPUT); // 设置GPIO12为输出模式，用于控制LED
+    dht.begin();         // 初始化DHT传感器
 
+    // 初始化BLE设备
     BLEDevice::init("ESP32-BLE");
-    BLEServer *pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new MyServerCallbacks());
+    BLEServer *pServer = BLEDevice::createServer(); // 创建BLE服务器
+    pServer->setCallbacks(new MyServerCallbacks()); // 设置BLE服务器回调
 
-    BLEService *pService = pServer->createService(SERVICE_UUID);
-    pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY);
-    pCharacteristic->addDescriptor(new BLE2902());
+    BLEService *pService = pServer->createService(SERVICE_UUID);                                                  // 创建UART服务
+    pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_TX, BLECharacteristic::PROPERTY_NOTIFY); // 创建发送特征
+    pCharacteristic->addDescriptor(new BLE2902());                                                                // 添加BLE2902描述符，支持通知
 
-    BLECharacteristic *pCharacteristic_RX = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE);
-    pCharacteristic_RX->setCallbacks(new MyCallbacks());
+    BLECharacteristic *pCharacteristic_RX = pService->createCharacteristic(CHARACTERISTIC_UUID_RX, BLECharacteristic::PROPERTY_WRITE); // 创建接收特征
+    pCharacteristic_RX->setCallbacks(new MyCallbacks());                                                                               // 设置接收特征的回调
 
-    pService->start();
-    pServer->getAdvertising()->start();
+    pService->start();                  // 启动服务
+    pServer->getAdvertising()->start(); // 开始广播
     Serial.println("Waiting for client connection...");
 }
 
 void loop()
 {
+    // 如果设备已经连接，且需要发送DHT数据
     if (deviceConnected)
     {
         if (flag_send_dht)
         {
-            memset(BLEbuf, 0, 32);
-            sprintf(BLEbuf, "湿度:%.2f, 温度:%.2f", dht_buf[0], dht_buf[1]);
-            pCharacteristic->setValue(BLEbuf);
-            pCharacteristic->notify(); // Send the value to the app
+            memset(BLEbuf, 0, 32);                                           // 清空发送缓冲区
+            sprintf(BLEbuf, "湿度:%.2f, 温度:%.2f", dht_buf[0], dht_buf[1]); // 格式化湿度和温度数据
+            pCharacteristic->setValue(BLEbuf);                               // 设置发送的值
+            pCharacteristic->notify();                                       // 通知客户端接收数据
             Serial.print("*** Sent Value: ");
-            Serial.println(BLEbuf);
-            flag_send_dht = 0;
+            Serial.println(BLEbuf); // 打印发送的值
+            flag_send_dht = 0;      // 重置标志位
         }
     }
 
+    // 如果需要读取温湿度传感器
     if (flag_dht)
     {
-        delay(2000); // DHT11 sensor read delay
-        float h = dht.readHumidity();
-        float t = dht.readTemperature();
-        dht_buf[0] = h;
-        dht_buf[1] = t;
+        delay(2000); // 等待2秒以便传感器读取
+
+        float h = dht.readHumidity();    // 读取湿度
+        float t = dht.readTemperature(); // 读取温度
+        dht_buf[0] = h;                  // 保存湿度数据
+        dht_buf[1] = t;                  // 保存温度数据
 
         if (isnan(h) || isnan(t))
-        {
+        { // 如果读取失败
             Serial.println("读取DHT传感器失败");
         }
         else
         {
-            Serial.printf("湿度: %.2f %%\t 温度: %.2f °C\n", h, t);
-            flag_send_dht = 1;
-            flag_dht = 0;
+            Serial.printf("湿度: %.2f %%\t 温度: %.2f °C\n", h, t); // 打印读取的值
+            flag_send_dht = 1;                                      // 设置发送数据的标志位
+            flag_dht = 0;                                           // 重置读取传感器的标志位
         }
     }
 
+    // 如果需要点亮LED
     if (flag_led)
     {
-        digitalWrite(12, HIGH); // Turn LED on
-        delay(1000);            // Wait for a second
-        digitalWrite(12, LOW);  // Turn LED off
-        delay(1000);            // Wait for a second
-        flag_led = 0;
+        digitalWrite(12, HIGH); // 点亮LED
+        delay(1000);            // 等待1秒
+        digitalWrite(12, LOW);  // 熄灭LED
+        delay(1000);            // 等待1秒
+        flag_led = 0;           // 重置LED控制标志位
     }
-    delay(1000); // Main loop delay
+
+    delay(1000); // 主循环延时1秒
 }
